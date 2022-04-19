f62ad8d8ea91ea31ad4aa977f1311e985515bace
YM Ng <C110156158@nkust.edu.tw>
2022-04-19 16:04:15 +0800

    third commit

--- 39.py
+++ 39.py
@@ -1,1 +1,16 @@
-
+w = 3
+for i in range(0,4):
+    a=''
+    for j in range(0,w-i):
+        a += " "
+    for k in range(0,2*i+1):
+        a+='*'
+    print(a)
+g = 4
+for i in range(3,0,-1):
+    a=''
+    for j in range(0,g-i):
+        a += " "
+    for k in range(0,2*i-1):
+        a+='*'
+    print(a)
